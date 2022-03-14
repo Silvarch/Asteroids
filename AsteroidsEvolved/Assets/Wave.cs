2ae7e7032a01ea4714bf34147c75c4d2ce3be559
Logan <logan.larocque@snhu.edu>
2022-03-14 14:53:15 -0700

    Basic spawners added
    basic spawners created. need to add functionality to spawn incrementally more asteroids. Should be complete by EOD 3/15/2022

---
+++ AsteroidsEvolved/Assets/Wave.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Wave
+{
+    public Transform enemy;
+    public int Count;
+    public float SpawnRate;
+}
