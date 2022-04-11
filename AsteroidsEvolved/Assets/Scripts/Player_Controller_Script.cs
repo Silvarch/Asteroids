e69badcf4a3aef221b75e308f234ba2cf52bfb08
Logan <logan.larocque@snhu.edu>
2022-04-11 12:57:39 -0700

    Revert "Merge branch 'main' of https://github.com/Silvarch/Asteroids"
    This reverts commit 901f499421f5a1a37f69c8a5004e110756f03014, reversing
    changes made to 80d8f644457a031fcb810c126c80b9b5c732c89a.

--- AsteroidsEvolved/Assets/Scripts/Player_Controller_Script.cs
+++ AsteroidsEvolved/Assets/Scripts/Player_Controller_Script.cs
@@ -9,7 +9,6 @@ public class Player_Controller_Script : MonoBehaviour
     // Start is called before the first frame update
     public float moveSpeed;
     public float rotationSpeed;
-    public LivesHandler playerLife;
 
     private Rigidbody2D rd;
     private Vector2 moveVelocity;
@@ -22,7 +21,7 @@ public class Player_Controller_Script : MonoBehaviour
     void Start()
     {
         spawnPoint = GameObject.Find("SpawnPoint").transform.position;
-        rd = this.GetComponent<Rigidbody2D>();
+        rd = GetComponent<Rigidbody2D>();
         this.transform.position = spawnPoint;
     }
 
@@ -43,12 +42,11 @@ public class Player_Controller_Script : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag == "Asteroid" || collision.tag == "HugeAsteroid") 
+        if (collision.tag == "Asteroid") 
         {
             //Debug.Log("Ran into an asteroid");
             moveVelocity = new Vector2(0, 0);
             rd.transform.position = spawnPoint;
-            playerLife.decreaseLife();
         }
     }
 
